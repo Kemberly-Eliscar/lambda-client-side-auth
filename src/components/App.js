1b371910a5e9c1b8b07fbf9b96880e8d8a258200
Kemberly Eliscar <kemberlyeliscar@Kemberlys-Air-3.home>
2020-01-24 12:00:58 -0500

    set up private route

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
 import "./App.css"
 import React from "react"
-
-
+import { Link, Route } from "react-router-dom"
+import Signin from "./Signin"
+import Account from "./Account"
+import ProtectedRoute from "./ProtectedRoute"
 
 
 function App() {
 	return (
 		<div className="wrapper">
-		<h1>Welcome</h1>
+			<nav>
+				<Link to="/">Home</Link>
+				<Link to="/signin">Sign In</Link>
+				<Link to="/account">My Account</Link>
+			</nav>
+
+			<Route exact path="/signin" component={Signin} />
+			<ProtectedRoute exact path="/account" component={Account} />
 		</div>
 	)
 }
 
-export default(App)
+export default App
---
+++ src/components/Signin.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import api from "../utils/api"
+
+
+function Signin(props) {
+
+    //THIS IS OPTIONAL ERROR MESSAGE FUNCTION
+    const [error, setError] = useState()
+
+    //create an object to set state
+    const [data, setData] = useState({
+        //define initial value
+        email: "",
+        password: "",
+    })
+    
+    //handle change function for those inputs
+    //a function that takes in an event as their parameters
+    //we need to take target name and value to set state
+    const handleChange = (event) => {
+        setData({
+            //set an immutable object by using a spread operator ...
+            ...data,
+            [event.target.name]: event.target.value,
+        })
+    }
+
+    // we need to set up a handleSubmit function
+    const handleSubmit =(event) => {
+            event.preventDefault()
+            // post request because we are sending data
+            api()
+            .post("/signin", data)
+            .then(result => {
+                console.log(result.data)
+                localStorage.setItem("token", result.data.token)
+            })
+            .catch(err => {
+                setError(err.response.data.message)
+            })
+        }
+    
+
+        //last step we need to attach the values and keys and handle change functions to each one of our inputs
+    return (
+        <form onSubmit={handleSubmit}>
+            {error && <div className="error"> {error}</div>} 
+
+            <input type="email" name="email" placeholder="Email" value={data.email} onChange={handleChange}/>
+            <input type="password" name="password" placeholder="Password" value={data.password} onChange={handleChange} />
+
+            <button type="submit">Sign In</button>
+        </form>
+    )
+}
+
+export default Signin
