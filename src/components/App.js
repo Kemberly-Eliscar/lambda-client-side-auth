e91d8153daefb7de2d2d2872bb06b28a457e151e
Kemberly Eliscar <kemberlyeliscar@Kemberlys-Air-3.home>
2020-01-24 10:49:12 -0500

    set up my environment and downloaded dependencies

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,15 @@
 import "./App.css"
 import React from "react"
-import { Link, Route, withRouter } from "react-router-dom"
-import { getToken } from "../utils/api"
-import ProtectedRoute from "./ProtectedRoute"
-import Signin from "./Signin"
-import Account from "./Account"
-import Logout from "./Logout"
+
+
+
 
 function App() {
-	const signedIn = getToken()
-
 	return (
 		<div className="wrapper">
-			<nav>
-				<Link to="/">Home</Link>
-
-				{/* We can conditionally show links if logged in or not */}
-				{!signedIn && <Link to="/signin">Sign In</Link>}
-				{signedIn && <Link to="/account">My Account</Link>}
-				{signedIn && <Link to="/logout">Logout</Link>}
-			</nav>
-
-			<Route exact path="/signin" component={Signin} />
-			{/* These routes will require an auth token to be set, due to our handy HOC */}
-			<ProtectedRoute exact path="/account" component={Account} />
-			<ProtectedRoute exact path="/logout" component={Logout} />
+		<h1>Welcome</h1>
 		</div>
 	)
 }
 
-export default withRouter(App)
+export default(App)
